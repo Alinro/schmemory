37c37c3d8a7f4e2e45158099b0078641496075ce
Alin Dumitru <alin.dumitru20@gmail.com>
2024-10-06 13:15:00 +0200

    fix: tests after two player mode implementation

--- client/src/game/Board/Board.test.ts
+++ client/src/game/Board/Board.test.ts
@@ -6,19 +6,21 @@ import Board from "./Board";
 test("flip the same cards, score is increased", () => {
   const board = new Board(Card, new Tracker(document.body), 10);
 
-  board.cards[0].onFlip();
-  board.cards[10].onFlip();
+  board.cards[0].Element.click();
+  board.cards[10].Element.click();
 
   // it would probably more correct to check the onScoreChanged method was called
   // instead of checking the value directly
-  expect(board.Tracker.Score).toBe(1);
+  expect(board.Tracker.Score["Player 1"]).toBe(1);
+  expect(board.Tracker.Score["Player 2"]).toBe(0);
 });
 
 test("flip different cards, score is not increased", () => {
   const board = new Board(Card, new Tracker(document.body), 10);
 
-  board.cards[0].onFlip();
-  board.cards[1].onFlip();
+  board.cards[0].Element.click();
+  board.cards[1].Element.click();
 
-  expect(board.Tracker.Score).toBe(0);
+  expect(board.Tracker.Score["Player 1"]).toBe(0);
+  expect(board.Tracker.Score["Player 2"]).toBe(0);
 });
